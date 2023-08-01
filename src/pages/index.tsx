4a83d0225a6d77634c76920f498357ce08223dc9
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2023-08-02 00:37:04 +0300

    update project global pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useUser } from 'src/context/UserContext';
+import { ELocalStorageKeys } from 'src/utils/app-contants';
+import { ROUTES } from 'src/utils/app-routes';
 
 export default function Home() {
   const router = useRouter();
+  const { user } = useUser();
 
   useEffect(() => {
-    // this may cause problems ...redirect to login ?
-    // TODO: convert to protected routes
-    router.push('/user/login');
-  }, []);
+    const _business = localStorage.getItem(ELocalStorageKeys.USER_LOCATIONS) as any;
+    const _username = localStorage.getItem(ELocalStorageKeys.USER_NAME) as any;
+    const _level = localStorage.getItem(ELocalStorageKeys.LEVELS) as any;
 
-  // removed part could be get from previous commits
+    if (!user) router.push(ROUTES.AUTH);
+    else if (_level === 'user') router.push('/shop/' + _business[0].value);
+    else router.push('/' + _username + '/business');
+  }, [user]);
+
   return <></>;
 }
