85002bb44d09880bdc97a4560319eea87012f7d7
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-08-10 04:50:15 +0300

    migrating: customers, units, brands, taxes, ...

--- src/models/pos.types.ts
+++ src/models/pos.types.ts
@@ -31,20 +31,20 @@ export interface IProduct {
   name: string;
   business_id: any;
   location_id: number;
-  type: string;
+  Etype: string;
   is_tailoring: number;
-  is_service: number | boolean;
-  is_fabric: number | boolean;
+  is_service: 0 | 1 | boolean;
+  is_fabric: 0 | 1 | boolean;
   subproductname: string;
   unit_id: number;
   brand_id: number;
   category_id: number;
   sub_category_id: any;
   tax: any;
-  never_tax: number | boolean;
+  never_tax: 1 | 0 | boolean;
   alert_quantity: string;
   sku: string;
-  barcode_type: string;
+  Ebarcode_type: string;
   image: string;
   product_description: any;
   created_by: number;
@@ -61,7 +61,7 @@ export interface IProduct {
   stock: number;
   variations: any[];
   packages: IPackage[];
-  stocks: any[];
+  stocks: IStock[];
   category: ICategory;
 }
 
@@ -75,4 +75,184 @@ export interface IPackage {
   product_ids: any;
   created_by: number;
   created_at: any;
+}
+
+export interface ICustomer {
+  id: number;
+  location_id: number;
+  type: string;
+  first_name: string;
+  last_name: string;
+  name: any;
+  email: any;
+  contact_id: any;
+  contact_status: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  address_line_1?: string;
+  address_line_2?: string;
+  zip_code?: string;
+  mobile: string | number;
+  created_by: number;
+  shipping_address?: string;
+  deleted_at: any;
+  created_at: string;
+  updated_at?: string;
+}
+
+export interface IPackageType {
+  id: number;
+  name: string;
+  location_id: number;
+  created_by: number;
+  created_at: string | null;
+  multiple_value: string;
+  extras: string;
+}
+
+export interface IUnit {
+  id: number;
+  type: string;
+  name: string;
+  unit: string;
+  created_at: any;
+}
+
+export interface IUnits {
+  units: IUnit[];
+}
+
+export interface IBrand {
+  id: number;
+  location_id: number;
+  name: string;
+  description: string;
+  created_by: number;
+  tax_id: any;
+  never_tax: number;
+  deleted_at: any;
+  created_at: any;
+  updated_at: any;
+  use_for_repair: any;
+  products_count: number;
+  products: IProduct[];
+}
+
+export interface ITax {
+  id: number;
+  location_id: number;
+  name: string;
+  amount: number;
+  is_tax_group: number;
+  for_tax_group: number;
+  created_by: number;
+  woocommerce_tax_rate_id: any;
+  deleted_at: any;
+  created_at: any;
+  updated_at: any;
+  for_tax_inclusive: number;
+  for_tax_exclusive: number;
+  is_inc_or_exc: string;
+  Etype: 'percentage' | 'fixed';
+  is_primary: boolean | 0 | 1;
+  Etax_type: string;
+  tax_group: ITaxGroup[];
+}
+
+export interface ITaxGroup {
+  id: number;
+  location_id: number;
+  name: string;
+  amount: number;
+  is_tax_group: number;
+  for_tax_group: number;
+  created_by: number;
+  woocommerce_tax_rate_id: any;
+  deleted_at: any;
+  created_at: any;
+  updated_at: any;
+  for_tax_inclusive: number;
+  for_tax_exclusive: number;
+  is_inc_or_exc: string;
+  type: string;
+  is_primary: number;
+  tax_type: string;
+  pivot: IPivot;
+}
+
+export interface IPivot {
+  parent_id: number;
+  tax_id: number;
+}
+
+export interface ITaxes {
+  taxes: ITax[];
+}
+
+export interface ICurrency {
+  id: number;
+  country: string;
+  currency: string;
+  code: string;
+  symbol: string;
+  thousand_separator: string;
+  decimal_separator: string;
+  exchange_rate: string;
+  created_at: any;
+  updated_at: any;
+}
+
+export interface IPurchase {
+  id: number;
+  location_id: number;
+  type: string;
+  sub_type: any;
+  status: string;
+  sub_status: any;
+  is_quotation: number;
+  payment_status: string;
+  contact_id: number;
+  invoice_no: string;
+  ref_no: any;
+  tax_amount: string;
+  discount_type: string;
+  discount_amount: string;
+  notes: any;
+  total_price: string;
+  document: any;
+  exchange_rate: string;
+  created_by: number;
+  created_at: string;
+  updated_at: any;
+  total_taxes: string;
+  taxes: string;
+  currency_id: number;
+  products: IProduct[];
+  payment: IPayment;
+  stocks: IStock[];
+}
+
+export interface IPayment {
+  id: number;
+  transaction_id: number;
+  payment_type: string;
+  amount: string;
+  created_by: number;
+  created_at: string;
+  notes: any;
+}
+
+export interface IStock {
+  id: number;
+  transaction_id: number;
+  transaction_lines_id: number;
+  product_id: number;
+  variation_id: number;
+  qty_received: string;
+  qty_sold: string;
+  sold_at: string;
+  created_by: number;
+  created_at: string;
+  updated_at: any;
 }
--- src/services/pos.service.ts
+++ src/services/pos.service.ts
@@ -1,6 +1,18 @@
 import { type AxiosResponse } from 'axios';
 import { type ICustomResponse } from '@models/global.types';
-import { ICategory, IProduct } from '@models/pos.types';
+import {
+  ICategory,
+  IProduct,
+  ICustomer,
+  IPackageType,
+  IUnits,
+  IBrand,
+  ITaxes,
+  ITax,
+  ICurrency,
+  IPurchase,
+  IPayment,
+} from '@models/pos.types';
 import api from 'src/utils/app-api';
 import useSWR, { type SWRConfiguration } from 'swr';
 type TServiceResponse<T> = AxiosResponse<ICustomResponse<T>>;
@@ -16,48 +28,177 @@ interface ICreateProductPayload extends Partial<IProduct> {
   name: string;
   category_id: number;
   location_id: number;
-  type: 'single' | 'variable' | 'package' | 'tailoring_package';
-  is_service: boolean | number;
-  is_fabric: boolean | number;
+  Etype: 'single' | 'variable' | 'package' | 'tailoring_package';
+  is_service: boolean | 0 | 1;
+  is_fabric: boolean | 0 | 1;
   unit_id: number;
-  never_tax: boolean | number;
+  never_tax: boolean | 0 | 1;
   sku: string;
-  barcode_type: 'C128' | 'C39' | 'C93' | 'EAN8' | 'EAN13' | 'UPCA' | 'UPCE';
+  Ebarcode_type: 'C128' | 'C39' | 'C93' | 'EAN8' | 'EAN13' | 'UPCA' | 'UPCE';
   sell_price: number | string;
   cost_price: number | string;
 }
 
 interface IUpdateProductPayload extends Partial<IProduct> {}
 
+interface ICreateCustomerPayload extends Partial<ICustomer> {
+  first_name: string;
+  mobile: number | string;
+}
+
+interface ISearch {
+  search: string;
+}
+
+interface IUpdateCustomerPayload extends Partial<ICustomer> {}
+
+interface IGetPackageType extends Partial<IPackageType> {}
+
+interface ICreateBrandPayload extends Partial<IBrand> {
+  name: string;
+}
+
+interface IUpdateBrandPayload extends Partial<IBrand> {}
+
+interface ICreateTaxPayload extends Partial<ITax> {
+  name: string;
+  amount: number;
+  Etype: 'percentage' | 'fixed';
+  is_primary: boolean | 0 | 1;
+  Etax_type: 'primary' | 'group' | 'excise' | 'service';
+}
+
+interface IUpdateTaxPayload extends Partial<ITax> {}
+
+interface IUpdatePaymentPayload extends Partial<IPayment> {}
+
+interface ICreatePurchasePayload extends Partial<IPurchase> {
+  location_id: number;
+  Estatus: 'draft' | 'processing' | 'received' | 'partially_received' | 'cancelled';
+  Epayment_status: 'paid' | 'credit' | 'partially_paid' | 'due';
+  Epayment_type: 'cash' | 'card' | 'cheque' | 'bank';
+}
+
+interface IUpdatePurchasePayload extends Partial<IPurchase> {}
+
+interface IGetCloseRegisteration {
+  cash: number;
+  card: number;
+  cheque: number;
+  bank: number;
+}
+
 const posSetvice = {
-  getCategories: async () =>
-    api.get<any, TServiceResponse<ICategory[]>, any>('/categories').then((data) => data.data),
+  getCategories: async (locId: string) =>
+    api
+      .get<any, TServiceResponse<ICategory[]>, any>(`/categories/${locId}`)
+      .then((data) => data.data),
   getCategory: async (id: string) =>
-    api.get<any, TServiceResponse<ICategory>, any>(`/categories/${id}`).then((data) => data.data),
-  createCategory: async (payload: ICreateCategoryPayload) =>
-    api.post('/categories', payload).then((data) => data.data),
+    api
+      .get<any, TServiceResponse<ICategory>, any>(`/categories/${id}/show`)
+      .then((data) => data.data),
+  createCategory: async (locId: string, payload: ICreateCategoryPayload) =>
+    api.post(`/categories/${locId}`, payload).then((data) => data.data),
   updateCategory: async (id: string, payload: IUpdateCategoryPayload) =>
     api.put(`/categories/${id}`, payload).then((data) => data.data),
   deleteCategory: async (id: string) => api.delete(`/categories/${id}`).then((data) => data.data),
 
-  getProducts: async () =>
-    api.get<any, TServiceResponse<IProduct[]>, any>('/products').then((data) => data.data),
+  getProducts: async (locId: string) =>
+    api.get<any, TServiceResponse<IProduct[]>, any>(`/products/${locId}`).then((data) => data.data),
   getProduct: async (id: string) =>
-    api.get<any, TServiceResponse<IProduct>, any>(`/products/${id}`).then((data) => data.data),
+    api.get<any, TServiceResponse<IProduct>, any>(`/products/${id}/show`).then((data) => data.data),
+  getPackageTypes: async (locId: string) =>
+    api
+      .get<any, TServiceResponse<IGetPackageType>, any>(`/package-type/${locId}`)
+      .then((data) => data.data),
   createProduct: async (payload: ICreateProductPayload) =>
     api.post('/products', payload).then((data) => data.data),
   updateProduct: async (id: string, payload: IUpdateProductPayload) =>
     api.put(`/products/${id}`, payload).then((data) => data.data),
+  updateStatus: async (id: string, payload: { status: 'active' | 'inactive' }) =>
+    api.put(`/products/${id}/status`, payload).then((data) => data.data),
   deleteProduct: async (id: string) => api.delete(`/products/${id}`).then((data) => data.data),
-  transferProduct: async (id: string) =>
+  transfer: async (id: string) =>
     api
       .get<any, TServiceResponse<IProduct>, any>(`/products/${id}/transfer`)
       .then((data) => data.data),
+  import: async (locId: string) => api.post(`/products/${locId}/import`).then((data) => data.data),
+  // search: async (locId: string, payload: ISearch) =>
+  //   api
+  //     .get<any, TServiceResponse<IProduct>, any>(`/products/search/${locId}`, payload)
+  //     .then((data) => data.data),
+
+  getCustomers: async (locId: string) =>
+    api
+      .get<any, TServiceResponse<ICustomer[]>, any>(`/customers/${locId}`)
+      .then((data) => data.data),
+  getCustomer: async (id: string) =>
+    api
+      .get<any, TServiceResponse<ICustomer>, any>(`/customers/${id}/show`)
+      .then((data) => data.data),
+  createCustomer: async (locId: string, payload: ICreateCustomerPayload) =>
+    api.post(`/customers/${locId}`, payload).then((data) => data.data),
+  updateCustomer: async (id: string, payload: IUpdateCustomerPayload) =>
+    api.put(`/customers/${id}`, payload).then((data) => data.data),
+  deleteCustomer: async (id: string) => api.delete(`/customers/${id}`).then((data) => data.data),
+
+  getUnits: async () =>
+    api.get<any, TServiceResponse<IUnits>, any>(`/units`).then((data) => data.data),
+
+  getBrands: async (locId: string) =>
+    api.get<any, TServiceResponse<IBrand[]>, any>(`/brands/${locId}`).then((data) => data.data),
+  getBrand: async (id: string) =>
+    api.get<any, TServiceResponse<IBrand>, any>(`/brands/${id}/show`).then((data) => data.data),
+  createBrand: async (locId: string, payload: ICreateBrandPayload) =>
+    api.post(`/brands/${locId}`, payload).then((data) => data.data),
+  updateBrand: async (id: string, payload: IUpdateBrandPayload) =>
+    api.put(`/brands/${id}`, payload).then((data) => data.data),
+  deleteBrand: async (id: string) => api.delete(`/brands/${id}`).then((data) => data.data),
+
+  getTaxes: async (locId: string) =>
+    api.get<any, TServiceResponse<ITaxes>, any>(`/taxes/${locId}`).then((data) => data.data),
+  getTax: async (id: string) =>
+    api
+      .get<any, TServiceResponse<{ tax: ITax }>, any>(`/taxes/${id}/show`)
+      .then((data) => data.data),
+  createTax: async (locId: string, payload: ICreateTaxPayload) =>
+    api.post(`/taxes/${locId}`, payload).then((data) => data.data),
+  updateTax: async (id: string, payload: IUpdateTaxPayload) =>
+    api.put(`/taxes/${id}`, payload).then((data) => data.data),
+  deleteTax: async (id: string) => api.delete(`/taxes/${id}`).then((data) => data.data),
+
+  getCurrencies: async () =>
+    api.get<any, TServiceResponse<ICurrency[]>, any>(`/currencies`).then((data) => data.data),
+
+  getPurchases: async (locId: string) =>
+    api
+      .get<any, TServiceResponse<IPurchase[]>, any>(`/purchase/${locId}`)
+      .then((data) => data.data),
+  getPurchase: async (id: string) =>
+    api.get<any, TServiceResponse<IPurchase>, any>(`/purchase/${id}/show`),
+  createPurchase: async (locId: string, payload: ICreatePurchasePayload) =>
+    api.post(`/purchase/${locId}`, payload).then((data) => data.data),
+  updatePurchase: async (id: string, payload: IUpdatePurchasePayload) =>
+    api.put(`/purchase/${id}`, payload).then((data) => data.data),
+  deletePurchase: async (id: string) => api.delete(`/purchase/${id}`).then((data) => data.data),
+  updatePayment: async (id: string, payload: IUpdatePaymentPayload) =>
+    api.put(`/purchase/${id}/payment`, payload).then((data) => data.data),
+
+  closeRegisteration: async (locId: string, payload: { hand_cash?: number; note: string }) =>
+    api.post(`/registeration/${locId}/close`).then((data) => data.data),
+  openRegisteration: async (
+    locId: string,
+    payload?: {
+      hand_cash?: number;
+    }
+  ) => api.post(`/registeration/${locId}/open`).then((data) => data.data),
+  getCloseRegisteration: async (locId: string) =>
+    api.get<any, TServiceResponse<IGetCloseRegisteration>, any>(`/registeration/${locId}/close`),
 };
 
-export const useCategories = (config?: SWRConfiguration) => {
+export const useCategories = (locId: string, config?: SWRConfiguration) => {
   const { data, error, isLoading, mutate } = useSWR(
-    config?.suspense ? null : '/categories',
+    config?.suspense ? null : `/categories/${locId}`,
     posSetvice.getCategories,
     {
       ...config,
@@ -74,7 +215,7 @@ export const useCategories = (config?: SWRConfiguration) => {
 
 export const useGetCategory = (id: string, config?: SWRConfiguration) => {
   const { data, error, isLoading, mutate } = useSWR(
-    config?.suspense ? null : `/categories/${id}`,
+    config?.suspense ? null : `/categories/${id}/show`,
     () => posSetvice.getCategory(id),
     {
       ...config,
@@ -89,9 +230,9 @@ export const useGetCategory = (id: string, config?: SWRConfiguration) => {
   };
 };
 
-export const useProducts = (config?: SWRConfiguration) => {
+export const useProducts = (locId: string, config?: SWRConfiguration) => {
   const { data, error, isLoading, mutate } = useSWR(
-    config?.suspense ? null : '/products',
+    config?.suspense ? null : `/products/${locId}`,
     posSetvice.getProducts,
     {
       ...config,
@@ -107,7 +248,7 @@ export const useProducts = (config?: SWRConfiguration) => {
 
 export const useGetProduct = (id: string, config?: SWRConfiguration) => {
   const { data, error, isLoading, mutate } = useSWR(
-    config?.suspense ? null : `/products/${id}`,
+    config?.suspense ? null : `/products/${id}/show`,
     () => posSetvice.getProduct(id),
     {
       ...config,
@@ -121,16 +262,224 @@ export const useGetProduct = (id: string, config?: SWRConfiguration) => {
   };
 };
 
-export const useTransferProduct = (id: string, config?: SWRConfiguration) => {
+export const usePackageTypes = (locId: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/package-type/${locId}`,
+    posSetvice.getPackageTypes,
+    {
+      ...config,
+    }
+  );
+  return {
+    packageTypes: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useTransfer = (id: string, config?: SWRConfiguration) => {
   const { data, error, isLoading, mutate } = useSWR(
     config?.suspense ? null : `/products/${id}/transfer`,
-    () => posSetvice.transferProduct(id),
+    () => posSetvice.transfer(id),
     {
       ...config,
     }
   );
   return {
     product: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+// export const useSearch = (locId: string, config?: SWRConfiguration) => {
+//   const { data, error, isLoading, mutate } = useSWR(
+//     config?.suspense ? null : `/products/search/${locId}`,
+//     () => posSetvice.search(locId),
+//     {
+//       ...config,
+//     }
+//   );
+//   return {
+//     product: data?.result ?? [],
+//     isLoading,
+//     error,
+//     refetch: mutate,
+//   };
+// };
+
+export const useCustomers = (locId: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/customers/${locId}`,
+    posSetvice.getCustomers,
+    {
+      ...config,
+    }
+  );
+  return {
+    customers: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useGetCustomer = (id: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/customers/${id}/show`,
+    () => posSetvice.getCustomer(id),
+    {
+      ...config,
+    }
+  );
+  return {
+    customer: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useUnits = (config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/units`,
+    posSetvice.getUnits,
+    {
+      ...config,
+    }
+  );
+  return {
+    units: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useBrands = (locId: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/brands/${locId}`,
+    () => posSetvice.getBrands(locId),
+    {
+      ...config,
+    }
+  );
+  return {
+    brands: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useGetBrand = (id: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/brands/${id}/show`,
+    () => posSetvice.getBrand(id),
+    {
+      ...config,
+    }
+  );
+  return {
+    brand: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useTaxes = (locId: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/taxes/${locId}`,
+    () => posSetvice.getTaxes(locId),
+    {
+      ...config,
+    }
+  );
+  return {
+    taxes: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useGetTax = (id: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/taxes/${id}/show`,
+    () => posSetvice.getTax(id),
+    {
+      ...config,
+    }
+  );
+  return {
+    tax: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useCurrencies = (config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/currency`,
+    posSetvice.getCurrencies,
+    {
+      ...config,
+    }
+  );
+  return {
+    currencies: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const usePurchases = (locId: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/purchase/${locId}`,
+    () => posSetvice.getPurchases(locId),
+    {
+      ...config,
+    }
+  );
+  return {
+    purchases: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useGetPurchase = (id: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/purchase/${id}/show`,
+    () => posSetvice.getPurchase(id),
+    {
+      ...config,
+    }
+  );
+  return {
+    purchase: data?.result ?? [],
+    isLoading,
+    error,
+    refetch: mutate,
+  };
+};
+
+export const useGetCloseRegisteration = (locId: string, config?: SWRConfiguration) => {
+  const { data, error, isLoading, mutate } = useSWR(
+    config?.suspense ? null : `/registeration/${locId}/close`,
+    () => posSetvice.getCloseRegisteration(locId),
+    {
+      ...config,
+    }
+  );
+  return {
+    closeRegisteration: data?.result ?? [],
     isLoading,
     error,
     refetch: mutate,
