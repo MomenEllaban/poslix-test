447db771004860be1ece61bd0f24d8748de0dc03
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2023-09-28 17:27:02 +0300

    small updates

--- src/pages/shop/[id]/purchases/index.tsx
+++ src/pages/shop/[id]/purchases/index.tsx
@@ -91,40 +91,48 @@ const Purchases: NextPage = (props: any) => {
       disableExport: true,
       flex: 1,
       renderCell: ({ row }: Partial<GridRowParams>) => (
-        <>
-          <ButtonGroup className="mb-2 m-buttons-style">
-            <Button
-              disabled={row.status == 'draft'}
-              onClick={() => {
-                setPurchaseId(row.id);
-                setIsShowQtyManager(!isShowQtyManager);
-              }}>
-              <FontAwesomeIcon icon={faListCheck} />
+        <ButtonGroup className="mb-2 m-buttons-style">
+          <Button
+            disabled={row.status == 'draft'}
+            onClick={() => {
+              setPurchaseId(row.id);
+              setIsShowQtyManager(!isShowQtyManager);
+            }}>
+            <FontAwesomeIcon icon={faListCheck} />
+          </Button>
+          <Button
+            disabled={
+              row.status == 'draft' ||
+              row.payment_status == 'partially_paid' ||
+              row.payment_status == 'not_paid'
+            }
+            onClick={() => {
+              if (
+                row.status == 'draft' ||
+                row.payment_status == 'partially_paid' ||
+                row.payment_status == 'not_paid'
+              )
+                return;
+              setPurchaseId(row.id);
+              setIsShowPayments(!isShowPayments);
+            }}>
+            <FontAwesomeIcon icon={faDollarSign} />
+          </Button>
+          {permissions.hasEdit && (
+            <Button onClick={() => router.push('/shop/' + id + '/purchases/edit/' + row.id)}>
+              <FontAwesomeIcon icon={faPenToSquare} />
             </Button>
+          )}
+          {permissions.hasDelete && (
             <Button
-              disabled={row.status == 'draft'}
               onClick={() => {
-                setPurchaseId(row.id);
-                setIsShowPayments(!isShowPayments);
+                setSelectId(row.id);
+                setShow(true);
               }}>
-              <FontAwesomeIcon icon={faDollarSign} />
+              <FontAwesomeIcon icon={faTrash} />
             </Button>
-            {permissions.hasEdit && (
-              <Button onClick={() => router.push('/shop/' + id + '/purchases/edit/' + row.id)}>
-                <FontAwesomeIcon icon={faPenToSquare} />
-              </Button>
-            )}
-            {permissions.hasDelete && (
-              <Button
-                onClick={() => {
-                  setSelectId(row.id);
-                  setShow(true);
-                }}>
-                <FontAwesomeIcon icon={faTrash} />
-              </Button>
-            )}
-          </ButtonGroup>
-        </>
+          )}
+        </ButtonGroup>
       ),
     },
   ];
@@ -186,99 +194,89 @@ const Purchases: NextPage = (props: any) => {
   function getStatusStyle(status: string) {
     switch (status) {
       case 'paid':
-        return (
-          <div className="">
-            <span className="purchase-satus-style">{status}</span>
-          </div>
-        );
       case 'received':
-        return (
-          <div className="">
-            <span className="purchase-satus-style">{status}</span>
-          </div>
-        );
+        return <span className="purchase-satus-style">{status}</span>;
+
       default:
-        return status;
+        return <span className="purchase-satus-style paid-other">{status.split('_').join(' ')}</span>;
     }
   }
 
   return (
-    <>
-      <AdminLayout shopId={id}>
-        <ToastContainer />
-        <AlertDialog
-          alertShow={show}
-          alertFun={handleDeleteFuc}
-          shopId={id}
-          id={selectId}
-          url={'purchase'}>
-          Are you Sure You Want Delete This Item?
-        </AlertDialog>
-        {isShowQtyManager && (
-          <PurchasesQtyCheckList
-            selectedIndex={selectedIndex}
-            purchases={purchases}
-            locationSettings={locationSettings}
-            shopId={shopId}
-            purchaseId={purchaseId}
-            setIsShowQtyManager={setIsShowQtyManager}
-          />
-        )}
-        {isShowPayments && (
-          <PurchasePaymentsList
-            selectedIndex={selectedIndex}
-            purchases={purchases}
-            shopId={shopId}
-            purchaseId={purchaseId}
-            setIsShowPayments={setIsShowPayments}
-          />
-        )}
-        {!isShowQtyManager && !isShowPayments && (
-          <div className="row">
-            <div className="col-md-12">
-              {!isloading && permissions.hasInsert && (
-                <div className="mb-4">
-                  <button
-                    className="btn m-btn btn-primary p-3"
-                    onClick={() => {
-                      router.push('/shop/' + shopId + '/purchases/add');
-                    }}>
-                    <FontAwesomeIcon icon={faPlus} /> New Purchase{' '}
-                  </button>
+    <AdminLayout shopId={id}>
+      <ToastContainer />
+      <AlertDialog
+        alertShow={show}
+        alertFun={handleDeleteFuc}
+        shopId={id}
+        id={selectId}
+        url={'purchase'}>
+        Are you Sure You Want Delete This Item?
+      </AlertDialog>
+      {isShowQtyManager && (
+        <PurchasesQtyCheckList
+          selectedIndex={selectedIndex}
+          purchases={purchases}
+          locationSettings={locationSettings}
+          shopId={shopId}
+          purchaseId={purchaseId}
+          setIsShowQtyManager={setIsShowQtyManager}
+        />
+      )}
+      {isShowPayments && (
+        <PurchasePaymentsList
+          selectedIndex={selectedIndex}
+          purchases={purchases}
+          shopId={shopId}
+          purchaseId={purchaseId}
+          setIsShowPayments={setIsShowPayments}
+        />
+      )}
+      {!isShowQtyManager && !isShowPayments && (
+        <div className="row">
+          <div className="col-md-12">
+            {!isloading && permissions.hasInsert && (
+              <div className="mb-4">
+                <button
+                  className="btn m-btn btn-primary p-3"
+                  onClick={() => {
+                    router.push('/shop/' + shopId + '/purchases/add');
+                  }}>
+                  <FontAwesomeIcon icon={faPlus} /> New Purchase{' '}
+                </button>
+              </div>
+            )}
+            {!isloading ? (
+              <div>
+                <div className="page-content-style card">
+                  <h5>Product List</h5>
+                  <DataGrid
+                    className="datagrid-style"
+                    sx={{
+                      '.MuiDataGrid-columnSeparator': {
+                        display: 'none',
+                      },
+                      '&.MuiDataGrid-root': {
+                        border: 'none',
+                      },
+                    }}
+                    rows={purchases}
+                    columns={columns}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                    components={{ Toolbar: CustomToolbar }}
+                  />
                 </div>
-              )}
-              {!isloading ? (
-                <>
-                  <div className="page-content-style card">
-                    <h5>Product List</h5>
-                    <DataGrid
-                      className="datagrid-style"
-                      sx={{
-                        '.MuiDataGrid-columnSeparator': {
-                          display: 'none',
-                        },
-                        '&.MuiDataGrid-root': {
-                          border: 'none',
-                        },
-                      }}
-                      rows={purchases}
-                      columns={columns}
-                      pageSize={10}
-                      rowsPerPageOptions={[10]}
-                      components={{ Toolbar: CustomToolbar }}
-                    />
-                  </div>
-                </>
-              ) : (
-                <div className="d-flex justify-content-around">
-                  <Spinner animation="grow" />
-                </div>
-              )}
-            </div>
+              </div>
+            ) : (
+              <div className="d-flex justify-content-around">
+                <Spinner animation="grow" />
+              </div>
+            )}
           </div>
-        )}
-      </AdminLayout>
-    </>
+        </div>
+      )}
+    </AdminLayout>
   );
 };
 export default withAuth(Purchases);
