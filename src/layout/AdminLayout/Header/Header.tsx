e479d8ae6e7f6de3f8430b2a60c72ee6c51ca7d1
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2023-10-04 02:55:36 +0300

    replace text with logo

--- src/layout/AdminLayout/Header/Header.tsx
+++ src/layout/AdminLayout/Header/Header.tsx
@@ -12,6 +12,7 @@ import { RiEnglishInput } from 'react-icons/ri';
 import classNames from 'classnames';
 import { useDarkMode } from '../../../context/DarkModeContext';
 import DarkModeToggle from '../DarkModeToggle';
+import Image from 'next/image';
 
 type HeaderProps = {
   toggleSidebar: () => void;
@@ -50,7 +51,17 @@ export default function Header(props: HeaderProps) {
           <FontAwesomeIcon icon={faBars} />
         </Button>
         <Link href="/" className="header-brand d-md-none">
-          sidebar-brand-full
+          <div style={{ height: "18px", overflow: "hidden", }} className='d-flex flex-row'>
+            <Image
+
+              width={200}
+              height={150}
+              src='/images/logo1.png'
+              alt="sidebar-brand-full"
+              style={{ height: '100%', width: '100%' }}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
         </Link>
         <div className="header-nav d-none d-md-flex">
           <HeaderFeaturedNav />
