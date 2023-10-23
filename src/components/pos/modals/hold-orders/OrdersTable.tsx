f46250fe2efdad7228b4d69baaed931a9e014e5d
MohamedElsayed2202 <mohamed.elsayed.1.3.1999@gmail.com>
2023-10-23 10:43:01 +0300

    local

--- src/components/pos/modals/hold-orders/OrdersTable.tsx
+++ src/components/pos/modals/hold-orders/OrdersTable.tsx
@@ -1,7 +1,7 @@
 import { IReportData } from '@models/pos.types';
 import classNames from 'classnames';
 import Fuse from 'fuse.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
 import { MdAutorenew, MdInfoOutline } from 'react-icons/md';
 // import { FixedSizeList } from 'react-window';
@@ -13,6 +13,8 @@ import OrderInfoTable from './OrderInfoTable';
 import { motion } from 'framer-motion';
 import { useAppDispatch } from 'src/hooks';
 import { findAllData } from 'src/services/crud.api';
+import { DataGrid, GridColDef, GridRowParams } from '@mui/x-data-grid';
+import { ButtonGroup } from '@mui/material';
 
 export default function OrdersTable({ lang, shopId, searchQuery = '', closeModal }) {
   const dispatch = useAppDispatch();
@@ -22,7 +24,7 @@ export default function OrdersTable({ lang, shopId, searchQuery = '', closeModal
   const [orderId, setOrderId] = useState<string | number>('');
   const [page, setPage] = useState<number>(0);
   const [visibleItems, setvisibleItems] = useState<number>(10);
-
+  const [renderdItems, setRenderdItems] = useState<IReportData[]>([]);
   // this listing all orders once
   const { isLoading, salesReport } = useGetSalesReport(shopId, null, {});
 
@@ -31,70 +33,128 @@ export default function OrdersTable({ lang, shopId, searchQuery = '', closeModal
     setIsOrderDetails(true);
   };
 
-  const renderItems = () => {
-    if (!salesReport?.data?.length) return <></>;
+  // const renderItems = () => {
+  //   if (!salesReport?.data?.length) return [];
 
-    let orderlistPaginated = salesReport?.data;
-    const fuse = new Fuse(orderlistPaginated, {
-      threshold: 0.0,
-      ignoreLocation: true,
-      keys: ['id', 'contact_name'],
-    });
-    const result = fuse.search(searchQuery);
-    if (searchQuery) orderlistPaginated = result.map((r) => r.item);
+  //   let orderlistPaginated = salesReport?.data;
+  //   const fuse = new Fuse(orderlistPaginated, {
+  //     threshold: 0.0,
+  //     ignoreLocation: true,
+  //     keys: ['id', 'contact_name'],
+  //   });
+  //   const result = fuse.search(searchQuery);
+  //   if (searchQuery) orderlistPaginated = result.map((r) => r.item);
+  //   return orderlistPaginated?.slice(0, visibleItems);
+  // };
 
-    return orderlistPaginated?.slice(0, visibleItems).map((item, idx) => {
-      return (
-        <motion.tr
-          key={item.id + '-table-' + idx}
-          onViewportEnter={() => {
-            console.log('enter', idx, visibleItems);
-            if (idx === visibleItems - 3) {
-              setvisibleItems((p) => p + 10);
-              console.log('first');
-            }
-          }}>
-          <td>#{item.id}</td>
-          <td>{item.contact_name?.trim() || 'walk-in customer'}</td>
-          <td>{item.contact_mobile?.trim() || 'walk-in customer'}</td>
-          <td>
-            {Number(item.sub_total).toFixed(+locationSettings?.location_decimal_places)}{' '}
-            <span style={{ fontSize: '10px' }}>{locationSettings?.currency_code}</span>
-          </td>
-          <td>
-            <span className="d-flex flex-row gap-3">
-              <Button
-                variant="outline-info"
-                onClick={async () => {
-                  const res = await findAllData(`sales/${item.id}`)
-                  dispatch(addMultipleToCart({
-                    location_id: shopId, products: res.data.result.products,
-                    orderId: item.id, customerId: item.contact_id, lastTotal: item.sub_total,
-                    lastDue: +item.due
-                  }));
-                  closeModal()
-                }}>
-                <MdAutorenew />
-              </Button>
-              <Button variant="outline-info" onClick={() => handleOrderInfo(item.id)}>
-                <MdInfoOutline />
-              </Button>
-            </span>
-          </td>
-        </motion.tr>
-      );
-    });
-  };
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      headerName: '#',
+      minWidth: 50,
+      renderCell({ row }) {
+        return `#${row.id}`;
+      },
+    },
+    {
+      field: 'contact_name',
+      headerName: `${lang.cartComponent.orderModal.customer}`,
+      flex: 1,
+      renderCell({ row }) {
+        return row.contact_name !== ' ' ? row.contact_name : 'walk-in customer';
+      },
+    },
+    {
+      field: 'contact_mobile',
+      headerName: `${lang.cartComponent.orderModal.mobile}`,
+      flex: 1,
+      disableColumnMenu: true,
+    },
+    {
+      field: 'sub_total',
+      headerName: `${lang.cartComponent.orderModal.price}`,
+      flex: 1,
+      disableColumnMenu: true,
+    },
+    {
+      flex: 1,
+      field: 'action',
+      headerName: `${lang.cartComponent.orderModal.action}`,
+      filterable: false,
+      sortable: false,
+      disableExport: true,
+      renderCell: ({ row }: Partial<GridRowParams>) => (
+        <>
+          <ButtonGroup className="mb-2 m-buttons-style">
+            <Button
+              variant="outline-info"
+              onClick={async () => {
+                const res = await findAllData(`sales/${row.id}`);
+                dispatch(
+                  addMultipleToCart({
+                    location_id: shopId,
+                    products: res.data.result.products,
+                    orderId: row.id,
+                    customerId: row.contact_id,
+                    lastTotal: row.sub_total,
+                    lastDue: +row.due,
+                  })
+                );
+                closeModal();
+              }}>
+              <MdAutorenew />
+            </Button>
+            <Button variant="outline-info" onClick={() => handleOrderInfo(row.id)}>
+              <MdInfoOutline />
+            </Button>
+          </ButtonGroup>
+          {/*edited*/}
+          {/* <TestModal statusDialog={paymentModalShow} open={setPaymentModalShow}></TestModal> */}
+        </>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    if (salesReport.data.length === 0) setRenderdItems([]);
+    else {
+      let orderlistPaginated = salesReport?.data;
+      const fuse = new Fuse(orderlistPaginated, {
+        threshold: 0.0,
+        ignoreLocation: true,
+        keys: ['id', 'contact_name'],
+      });
+      const result = fuse.search(searchQuery);
+      if (searchQuery) orderlistPaginated = result.map((r) => r.item);
+      setRenderdItems(orderlistPaginated.slice(0, visibleItems));
+    }
+  }, [salesReport]);
 
   return (
     <>
-      <div
-        className="position-relative"
-        style={{
-          maxHeight: '60vh',
-          overflowY: 'auto',
-        }}>
-        <Table
+      {/* <div className="page-content-style card"> */}
+        <DataGrid
+          className="datagrid-style"
+          sx={{
+            '.MuiDataGrid-columnSeparator': {
+              display: 'none',
+            },
+            '&.MuiDataGrid-root': {
+              border: 'none',
+            },
+            minHeight: '100px'
+          }}
+          loading={isLoading}
+          rows={renderdItems}
+          columns={columns}
+          initialState={{
+            columns: { columnVisibilityModel: { mobile: false } },
+          }}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+        />
+      {/* </div> */}
+      {/* <Table
           responsive
           className={classNames({
             'd-none': isOrderDetails,
@@ -129,8 +189,8 @@ export default function OrdersTable({ lang, shopId, searchQuery = '', closeModal
 
           {!isLoading && (
             <table className="table table-centered table-hover align-middle  mb-0">
-              <tbody>
-                {/* {filteredOrdersList.length > 0 &&
+              <tbody> */}
+      {/* {filteredOrdersList.length > 0 &&
             [filteredOrdersList].map((ord: any, i) => {
               return (
                 <tr key={i}>
@@ -153,10 +213,10 @@ export default function OrdersTable({ lang, shopId, searchQuery = '', closeModal
                 </tr>
               );
             })} */}
-              </tbody>
+      {/* </tbody>
             </table>
-          )}
-          {/* {!isLoadingDetails && (
+          )} */}
+      {/* {!isLoadingDetails && (
       <table className="table table-centered table-hover align-middle  mb-0">
      <thead className="text-muted table-light">
           <tr>
@@ -197,8 +257,7 @@ export default function OrdersTable({ lang, shopId, searchQuery = '', closeModal
         </div>
       </table>
     )} */}
-        </Table>
-      </div>
+      {/* </Table> */}
       <OrderInfoTable
         shopId={shopId}
         orderId={orderId}
