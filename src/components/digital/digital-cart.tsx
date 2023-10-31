ad34b9f42d59a76d602be6c3724b969abcf568d8
MohamedElsayed2202 <mohamed.elsayed.1.3.1999@gmail.com>
2023-10-31 17:43:35 +0200

    digital localization

--- src/components/digital/digital-cart.tsx
+++ src/components/digital/digital-cart.tsx
@@ -3,8 +3,10 @@ import  CartItem  from "./CartItem"
 
 import { useDispatch,useSelector } from 'react-redux';
 import {addTodigitalCart ,incrementQuantity, decrementQuantity, removeItem} from '../../redux/slices/digitalCartSlice';
+import { useDigitalContext } from "src/modules/digital/_context/DigitalContext";
 const DigitalCart = ({cartItems,addItemTocart,removeFromCart,setRenderedScreen,totalPrice,location}) => {
     const {digitalCart} = useSelector((state:any) => state.digitalCart)
+    const { lang, setLang } = useDigitalContext();
     // const dispatch = useDispatch()
     // const getTotal = () => {
     //     let totalQuantity = 0
@@ -20,7 +22,7 @@ const DigitalCart = ({cartItems,addItemTocart,removeFromCart,setRenderedScreen,t
     return (
         <div className="digital-cart">
             <div className="digital-cart-items-container">
-                <h4>YOUR ORDER</h4>
+                <h4>{lang.digital.your_orders}</h4>
                 <div className="digital-cart-items-list">
                         {cartItems.length>=1?cartItems.map((item) => (
                         
@@ -39,7 +41,7 @@ const DigitalCart = ({cartItems,addItemTocart,removeFromCart,setRenderedScreen,t
                 />
 
 
-                 )):<p className="empty text-dark">Cart is empty</p>
+                 )):<p className="empty text-dark">{lang.digital.cart_empty}</p>
                  
                  }
 
@@ -47,8 +49,8 @@ const DigitalCart = ({cartItems,addItemTocart,removeFromCart,setRenderedScreen,t
                 </div>
             </div>
             <div className="digital-cart-checkout">
-                <Button disabled={!cartItems.length} className="checkout_btn" variant="contained" color="error" onClick={()=>setRenderedScreen('checkout')}>Checkout {totalPrice.toFixed(location?.location_decimal_places||2)} {location?.currency_code}</Button>
-                <Button >APPLY COUPON</Button>
+                <Button disabled={!cartItems.length} className="checkout_btn" variant="contained" color="error" onClick={()=>setRenderedScreen('checkout')}>{lang.pos.cartComponent.checkout} {totalPrice.toFixed(location?.location_decimal_places||2)} {location?.currency_code}</Button>
+                <Button >{lang.digital.apply_coupon}</Button>
             </div>
         </div>
     )
