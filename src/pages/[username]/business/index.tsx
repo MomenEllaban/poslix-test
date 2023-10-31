65bede3914bb6abd3926ca0df94e152129668039
elalfymohamed <elalfy.mohamed19@gmail.com>
2023-11-01 01:09:44 +0200

    Update business page - delete useEffect

--- src/pages/[username]/business/index.tsx
+++ src/pages/[username]/business/index.tsx
@@ -25,9 +25,6 @@ const MyBusinessesPage = () => {
   useEffect(() => {
     refetch();
   }, [router.asPath]);
-  useEffect(() => {
-    console.log(businessList);
-  });
 
   return (
     <OwnerAdminLayout>
