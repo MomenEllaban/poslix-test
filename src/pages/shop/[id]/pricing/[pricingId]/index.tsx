dd350690340b01be49681c58acb3a8aa7d8ff2e5
Ali Mohamed <aliiimhmddd99@gmail.com>
2023-11-01 22:23:16 +0200

    validate appearance inputs

--- src/pages/shop/[id]/pricing/[pricingId]/index.tsx
+++ src/pages/shop/[id]/pricing/[pricingId]/index.tsx
@@ -6,9 +6,6 @@ import * as cookie from 'cookie';
 import { Button, ButtonGroup, Spinner, ToastContainer } from 'react-bootstrap';
 import { apiFetchCtr } from 'src/libs/dbUtils';
 import { DataGrid, GridColDef, GridRowParams, GridToolbarContainer } from '@mui/x-data-grid';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { ROUTES } from 'src/utils/app-routes';
 import { useRouter } from 'next/router';
 import { findAllData, updateData } from 'src/services/crud.api';
 import { Toastify } from 'src/libs/allToasts';
@@ -119,22 +116,23 @@ const PricingGroup = (props: any) => {
   }
   // ----------------------------------------------------------------------------------------------
   const updatePricing = async () => {
+    
     const url = 'pricing-group'
    let  products=JSON.parse(JSON.stringify(productsPricingGroup))
   //  compose products with variants for backend req
-  products = products.filter((item) => {
-    if (item.parent_id) {
-      const parent = products.find((parentItem) => parentItem.id === item.parent_id);
-      if (parent) {
-        if (!parent.variants) {
-          parent.variants = [];
-        }
-        parent.variants.push({id:item.id,price:+item.price});
-      }
-      return false; // Remove the child object from the updated array
-    }
-    return true; // Keep the parent object in the updated array
-  });
+  // products = products.filter((item) => {
+  //   if (item.parent_id) {
+  //     const parent = products.find((parentItem) => parentItem.id === item.parent_id);
+  //     if (parent) {
+  //       if (!parent.variants) {
+  //         parent.variants = [];
+  //       }
+  //       parent.variants.push({id:item.id,price:+item.price});
+  //     }
+  //     return false; // Remove the child object from the updated array
+  //   }
+  //   return true; // Keep the parent object in the updated array
+  // });
   
     const data = {
       "location_id": router.query.id,
@@ -143,6 +141,7 @@ const PricingGroup = (props: any) => {
 
 
     try {
+      return
       const res = await updateData(url, router.query.pricingId, data)
       Toastify('success', 'Groups updated successfully')
 
@@ -162,11 +161,31 @@ const PricingGroup = (props: any) => {
       const res: any = await findAllData(`products/${router.query.id}?all_data=1`);
       const res2: any = await findAllData(`pricing-group/${router.query.id}?group_id=${router.query.pricingId}`);
       // set pricing for grop with old pricing if exist if not set it with the prodact normal sell price
-      
-      setProductsPricingGroup(res2?.data?.result?.products.map((p: any) => ({
+      let res2ResultProductsData=JSON.parse(JSON.stringify(res2?.data?.result?.products));
+      let res2ResultVariantsData=JSON.parse(JSON.stringify(res2?.data?.result?.variants));
+
+      let respons2Products=res2ResultProductsData.map((p: any) => ({
         id: p.id,
         price: p.pivot.price ? p.pivot.price : res?.data?.result.sell_price
-      })));
+      }))
+      
+       let respons2Variants=res2ResultVariantsData?.filter(el=>el.product).map((v: any) => ({
+        
+        parent_id:v.product.product_id,
+        id: v.product.id,
+        price: v.pivot.price ? v.pivot.price : res?.data?.result.sell_price
+      }))
+
+      // console.log(res2?.data?.result?.variants,'respons2Variants');
+      
+      setProductsPricingGroup([...respons2Variants,...respons2Products]);
+      // console.log([...respons2Variants,...respons2Products]);
+      
+    // setProductsPricingGroup(    res2?.data?.result?.products?.map((p: any) => ({
+    //     id: p.id,
+    //     price: p.pivot.price ? p.pivot.price : res?.data?.result.sell_price
+    //   })))
+      
       // compose products with variants
       let productsData = [...res?.data?.result];
 
@@ -190,6 +209,7 @@ const PricingGroup = (props: any) => {
 
           // Insert the variation objects after the parent object
           productsData.splice(parentIndex + 1, 0, ...variableProducts);
+          variableProducts.filter(v=>![...respons2Variants,...respons2Products].find(el=>el.id===v.id))
           setProductsPricingGroup(prev=>{
             return [...prev,...variableProducts]
           })
@@ -206,6 +226,8 @@ const PricingGroup = (props: any) => {
 
     } catch (e) {
       Toastify('error', 'Something went wrong')
+      console.log(e);
+      
     }
 
 
