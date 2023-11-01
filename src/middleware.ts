af5baed04ba558b782e36504b7458c0a9a05ed5b
elalfymohamed <elalfy.mohamed19@gmail.com>
2023-11-01 02:02:21 +0200

    handle middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,26 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+  const { cookies } = request;
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith('/_next')) return NextResponse.next();
+
+  const hasToken = cookies.get('next-auth.session-token')?.value ?? undefined;
+
+  if (!hasToken) {
+    if (pathname !== '/user/auth' && !pathname.includes('/api/auth')) {
+      request.nextUrl.pathname = '/user/auth';
+      return NextResponse.redirect(request.nextUrl);
+    }
+  }
+
+  if (hasToken) {
+    if (pathname === '/user/auth') {
+      request.nextUrl.pathname = '/';
+      return NextResponse.redirect(request.nextUrl);
+    }
+  }
+
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/:path*'],
-};
