7ada4578900c3a93b9d497096c554fe15a15ec3e
elalfymohamed <elalfy.mohamed19@gmail.com>
2023-11-13 03:04:04 +0200

    handle Date

--- src/components/filters/Date.tsx
+++ src/components/filters/Date.tsx
@@ -36,6 +36,12 @@ const lastFinancialYear = [
   new Date(today.getFullYear(), 2, 31),
 ];
 
+type Option = {
+  label: string;
+  range: string | undefined[] | Date[];
+  hidden?: boolean;
+};
+
 const options = [
   { label: 'Today', range: [today, today] },
   { label: 'Yesterday', range: [yesterday, yesterday] },
@@ -49,7 +55,22 @@ const options = [
   { label: 'Current financial year', range: currentFinancialYear },
   { label: 'Last financial year', range: lastFinancialYear },
   { label: 'Date', range: [], hidden: true },
-];
+] as Option[];
+
+const optionsValueText = [
+  { label: 'Today', range: 'today' },
+  { label: 'Yesterday', range: 'yesterday' },
+  { label: 'Last 7 days', range: 'last_7_days' },
+  { label: 'Last 30 days', range: 'last_30_days' },
+  { label: 'This month', range: 'this_month' },
+  { label: 'Last month', range: 'last_month' },
+  { label: 'This month last year', range: 'this_month_last_year' },
+  { label: 'This year', range: 'this year' },
+  { label: 'Last year', range: 'last_year' },
+  { label: 'Current financial year', range: 'current_financial_year' },
+  { label: 'Last financial year', range: 'last_financial_year' },
+  { label: 'Date', range: [], hidden: true },
+] as Option[];
 // Eslam 20
 export default function DatePicker({
   setStrSelectedDate,
@@ -57,24 +78,29 @@ export default function DatePicker({
   setSelectedRange,
   hiden = false,
   placeHolder = null,
+  valueTextRange = false,
 }) {
   const [startDatepicker, setStartDatepicker] = useState<Date | null>();
   const [endDatepicker, setEndDatepicker] = useState<Date | null>();
   const [label, setLabel] = useState(null);
 
-  const handleOptionClick = (range) => {
+  const handleOptionClick = (range: string | Date[]) => {
     setStartDatepicker(null);
     setEndDatepicker(null);
     setSelectedRange(range);
   };
 
   const renderOptions = () => {
-    return options.map((option, index) => (
+    const data = valueTextRange ? optionsValueText : options;
+    return data.map((option: Option, index: number) => (
       <MenuItem
         style={{ display: option.hidden ? 'none' : 'block' }}
         value={option.label}
         key={index}
-        onClick={() => handleOptionClick(option.range)}>
+        onClick={() => {
+          handleOptionClick(option.range);
+          setLabel(option.label);
+        }}>
         {option.label}
       </MenuItem>
     ));
@@ -85,24 +111,35 @@ export default function DatePicker({
       setStrSelectedDate([]);
       setLabel(null);
     } else {
+      if (valueTextRange) {
+        return;
+      }
       const startDate = selectedRange[0].toLocaleDateString();
       const endDate = selectedRange[1].toLocaleDateString();
-
       if (startDate == endDate) {
         setStrSelectedDate([startDate]);
       } else {
         setStrSelectedDate([startDate, endDate]);
       }
-
       // set select component label
-      const selectedVal = options.find((option) => option.range[0] === selectedRange[0]);
-      setLabel(selectedVal?.label);
+      // const selectedVal = options.find((option) => option.range[0] === selectedRange[0]);
+      // setLabel(selectedVal?.label);
     }
   }, [selectedRange]);
+
+  const handleFormatDate = (date: Date) => {
+    const _date = new Date(date);
+    return _date.toLocaleDateString('en-US');
+  };
 
   const onChange = ([start, end]) => {
     setStartDatepicker(start);
     setEndDatepicker(end);
+    if (valueTextRange) {
+      setStrSelectedDate([handleFormatDate(start), handleFormatDate(end)]);
+      return;
+    }
+
     if (end) setSelectedRange([start, end]);
   };
   return (
