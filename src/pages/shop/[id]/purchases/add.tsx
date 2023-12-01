4b725891bcaf07f779cabecf986b394190287aa6
elalfymohamed <elalfy.mohamed19@gmail.com>
2023-12-01 18:27:34 +0200

    fix bug

--- src/pages/shop/[id]/purchases/add.tsx
+++ src/pages/shop/[id]/purchases/add.tsx
@@ -520,6 +520,7 @@ const AddPurchase: NextPage = ({ shopId, id: editId }: any) => {
         onCostClick,
         setSelecetdId,
         setOpenRemoveDialog,
+        t
       }),
     [locationSettings, formObj, formObjRef]
   );
